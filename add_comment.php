c1cf005ac73f6ac32d9bf0c5ce75cecfe9710fea
Scheinin <Scheinin@163.com>
2017-05-09 21:00:30 +0800

    知问

---
+++ add_comment.php
@@ -0,0 +1,15 @@
+<?php
+
+		sleep(1);
+
+		require 'config.php';
+
+		$query = "INSERT INTO comment (titleid, comment, user, date) value ('{$_POST['titleid']}', '{$_POST['comment']}', '{$_POST['user']}', NOW())";
+
+		mysql_query($query) or die('新增失败!'.mysql_error());
+
+		echo mysql_affected_rows();
+
+		mysql_close();
+
+?>
